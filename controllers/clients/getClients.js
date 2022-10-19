b037229be48d52dbfdbe8e1572af706e9b82bf83
Maryna Dubnianskaya <dubnianskaya.marina@gmail.com>
2022-10-20 01:32:53 +0300

    some fixes

--- controllers/clients/getClients.js
+++ controllers/clients/getClients.js
@@ -1,15 +1,15 @@
 const connection = require("../../helpers/database");
+const { createError } = require('http-errors');
 
 const getClients = async (req, res) => {
     const sqlQuery = 'SELECT id, createdAt, updatedAt, name, mail, message FROM test';
-    const rows  = await connection.query(sqlQuery)
-    res.json({
-        status: 'success',
-        code: 200,
-        data: {
-            rows
-        },
-      });
+    await connection.query(sqlQuery, (err, result) => {
+        if (err) {
+            throw createError(404, "Request error")
+        } else {
+            res.send(result)
+        }
+    })
 };
     
 module.exports = getClients;
